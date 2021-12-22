c78fe2b1f6d4f42a9e6bed874f6ebf104f760828
赵华 <zhaohua@sheencity.com>
2021-12-22 11:16:48 +0800

    feat: 添加 router 入口文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAxios from "vue-axios";
+import App from "./App";
+import router from "./router";
 
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
