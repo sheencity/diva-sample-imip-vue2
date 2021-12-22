afc547666dc9dc51bc69a2f1f9c776ec32c24e27
赵华 <zhaohua@sheencity.com>
2021-12-22 11:37:46 +0800

    feat: 添加介绍页组件

---
+++ src/views/main/introduction/introduction.vue
@@ -0,0 +1,147 @@
+<template>
+  <article class="space-between">
+    <aside class="space-left all">
+      <app-basic-info :data="basicInfoData"></app-basic-info>
+      <app-card-table :data="cardTableData"></app-card-table>
+      <pie-charts :data="pieChartsData"></pie-charts>
+    </aside>
+    <aside class="space-right all">
+      <app-card-list :data="cardListData"></app-card-list>
+    </aside>
+  </article>
+</template>
+
+<script>
+import AppDialog from "../components/dialog/dialog";
+import AppBasicInfo from '../../components/common/basic-info/index';
+import AppCardTable from '../../components/common/basic-info/index';
+import PieCharts from '../../components/common/charts/pie/index';
+import AppCardList from '../../components/common/card-list/index';
+import AppAreaModule from "../components/area-module/area-module";
+
+export default {
+  data() {
+    return {
+      basicInfoData: null,
+      cardTableData: null,
+      pieChartsData: null,
+      cardListData: null,
+    };
+  },
+  created() {
+    
+  },
+  async mounted() {
+    
+  },
+  methods: {
+    init() {
+      this.getConfig()
+      this.initScene()
+    },
+    getConfig() {
+      // const config = axios.get(url)
+      // this.basicInfoData = config.basicInfoData;
+      // this.cardTableData = config.cardTableData;
+    },
+    initScene(name) {
+
+    }
+  },
+  beforeDestroy(){
+    this.modelEventList?.forEach((model)=>{
+      model.removeEventListener('click',this.addEvent);
+    })
+  },
+  destroyed(){
+    this.setFloorReset(this.floorList)
+  },
+  components: {
+    AppDialog,
+    AppAreaModule,
+    AppBasicInfo,
+    AppCardTable,
+    AppCardList,
+    PieCharts
+  },
+};
+</script>
+<style lang="scss" scoped>
+.module-1 {
+  .image {
+    width: 100%;
+    margin-bottom: .63rem;
+  }
+  p {
+    font-size: .88rem;
+    letter-spacing: .06rem;
+    color: #ebebeb;
+  }
+}
+
+.module-2 {
+  .content-table {
+    width: 100%;
+    border-collapse: collapse;
+    td {
+      width: 50%;
+      padding-top: .5rem;
+      border-top: 0;
+      border-right: 1px solid rgba(235, 235, 235, 0.2);
+      border-bottom: 1px solid rgba(235, 235, 235, 0.2);
+      border-left: 0;
+      vertical-align: middle;
+      &:last-of-type {
+        border-right: 0;
+      }
+    }
+    .content-each {
+      margin-left: 1.25rem;
+      p {
+        font-size: .75rem;
+        line-height: 1.25rem;
+        margin-bottom: .38rem;
+        color: #ebebeb;
+      }
+      div {
+        margin-bottom: .63rem;
+        ::v-deep span {
+          font-family: Saira;
+          color: #00f7ff;
+          line-height: 1.25rem;
+          font-weight: 500;
+          font-size: 1.25rem;
+        }
+      }
+    }
+    // 因为组件内容默认有个上边距，而每个表格也需要上边距，消除冲突
+    tr:nth-child(1) {
+      td {
+        padding-top: 0px;
+      }
+    }
+  }
+}
+
+.module-4 {
+  .prod-area,
+  .life-area {
+    h3 {
+      padding-bottom: 0.6rem;
+      font-weight: 500;
+      font-size: 1rem;
+      line-height: 1.38rem;
+      text-align: center;
+      text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.5);
+      background: url("../assets/route1/title.png") no-repeat 50% 10%;
+    }
+  }
+  .life-area {
+    margin-top: 1rem;
+  }
+  .area-module:hover {
+    background: rgba(255, 255, 255, 0.1);
+    border-color: #00f7ff;
+  }
+}
+</style>
---
+++ src/views/main/main.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="win">
+    <main>
+      <!-- 头部导航 -->
+      <app-header></app-header>
+      <!-- 中心内容 -->
+      <router-view></router-view>
+      <!-- 底部路由 -->
+      <app-footer></app-footer>
+    </main>
+  </div>
+</template>
+<script>
+// 所有的组件都按照App[组件名]命名规则
+import AppHeader from "../components/header/header.vue";
+import AppFooter from "../components/footer/index.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.win {
+  main {
+    position: relative;
+    pointer-events: none;
+    width: 100%;
+    padding: 0px 30px;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    // pointer-events: none;
+    background: linear-gradient(
+        0deg,
+        rgba(0, 0, 0, 0) 0%,
+        rgba(0, 0, 0, 0.4) 100%
+      )
+      top/100% 170px no-repeat;
+  }
+}
+</style>
