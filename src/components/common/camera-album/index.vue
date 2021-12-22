a431ff670f94f8a74784179b46760f882c5fcd5c
陈志强 <chenzhiqiang@corp.sheencity.com>
2021-12-22 20:15:12 +0800

    fix: 相册组件优化

--- src/components/common/camera-album/index.vue
+++ src/components/common/camera-album/index.vue
@@ -11,11 +11,11 @@
       <aside>
         <div
           class='pic-container'
-          v-for='(camera, index) in item.content'
+          v-for='camera in item.content.data'
           :key='camera.text'
-          :ref="index"
+          @click="playCameraTrack(camera.diva)"
         >
-          <img :src="require('../../../assets/route4-2/' + item.header.icon)" />
+          <img :src="require('../../../assets/' + camera.picture)" />
           <span>{{ camera.text }}</span>
         </div>
       </aside>
@@ -25,6 +25,7 @@
 
 <script>
 import AppDialogHeader from "./dialog-header";
+import { diva } from 'services/global';
 
 export default {
   props: ["dataSource"],
@@ -33,7 +34,10 @@ export default {
 
     };
   },
-  created() {
+  methods: {
+    playCameraTrack(camera) {
+      diva.client.playCameraTrack(camera.camera_track_name);
+    }
   },
   components: {
     AppDialogHeader,
@@ -47,5 +51,27 @@ export default {
   background: rgba(4, 14, 29, 0.6);
   border-radius: 2px;
   padding: 16px 20px 20px 20px;
+
+  .pic-container {
+    position: relative;
+    width: 160px;
+    height: 90px;
+    margin-bottom: 12px;
+    img {
+      width: 100%;
+      height: 100%;
+      display: block;
+      pointer-events: none;
+    }
+    span {
+      position: absolute;
+      bottom: 4px;
+      width: 100%;
+      text-align: center;
+      z-index: 2;
+      text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.5);
+      pointer-events: none;
+    }
+  }
 }
 </style>
