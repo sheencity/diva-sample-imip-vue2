53b6b74b2eaf2915106a8093e0b5c4c00e951a05
陈志强 <chenzhiqiang@corp.sheencity.com>
2021-12-23 10:40:22 +0800

    fix: 修改 statistics-panel 组件

--- src/components/common/statistics-panel/index.vue
+++ src/components/common/statistics-panel/index.vue
@@ -1,9 +1,16 @@
 <template>
   <app-dialog :header="header">
     <template v-slot:dialog-article>
-      <div class="module-5">
-        <div class="statistics">
-          <div :key="item.icon" v-for="item in items" class="item">
+      <div class="statistic-panel">
+        <div v-if="rankingData.length" class="ranking">
+          <div v-for="item in rankingData" :key="item.title" class="item">
+            <p>{{ item.title }}</p>
+            <span :style="{ color: item.fontColor }">{{ item.number }}</span>
+            <img src="/assets/images/route4-1/1.png" />
+          </div>
+        </div>
+        <div v-if="statisticData.length" class="statistic">
+          <div :key="item.icon" v-for="item in statisticData" class="item">
             <img :src="'/assets/images/route3-1/' + item.icon" />
             <div>
               <p>{{ item.title }}</p>
@@ -21,23 +28,54 @@
 <script>
 import AppDialog from "../dialog/dialog.vue";
 export default {
-  props: ["items", 'dataSource'],
+  props: ["dataSource"],
   data() {
     return {
-      currentIndex: null,
-      header: null,
+      header: this.dataSource.header,
+      rankingData: [],
+      statisticData: [],
     };
   },
-  created() {},
-  mounted() {},
-  methods: {},
-  destroyed() {},
+  created() {
+    this.rankingData = this.dataSource.content.data.filter((v) => !v.icon);
+    this.statisticData = this.dataSource.content.data.filter((v) => !!v.icon);
+  },
   component: [AppDialog]
 };
 </script>
 <style lang="scss" scoped>
-.module-5 {
-  .statistics {
+.statistic-panel {
+  .ranking {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    .item {
+      width: 148px;
+      position: relative;
+      p {
+        text-align: center;
+        font-size: 14px;
+        line-height: 20px;
+        margin-bottom: 35px;
+      }
+      span {
+        position: absolute;
+        width: 100%;
+        font-size: 36px;
+        font-family: 'Saira';
+        font-weight: 700;
+        line-height: 20px;
+        letter-spacing: 1px;
+        text-align: center;
+        top: 40px;
+      }
+      img {
+        margin: 0 auto;
+        display: block;
+      }
+    }
+  }
+  .statistic {
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
