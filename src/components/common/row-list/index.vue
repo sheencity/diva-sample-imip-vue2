a19e38a5522b2bdf2e846acdc0bcc115322f2265
程健 <chengjian@sheencity.com>
2022-01-27 10:38:36 +0800

    fix: 修复图片路径错误

--- src/components/common/row-list/index.vue
+++ src/components/common/row-list/index.vue
@@ -26,7 +26,7 @@
             <div class="closeBtn">
               <img
                 @click.stop="closeTopFrame()"
-                src="public/assets/images/icon/close.svg"
+                src="assets/images/icon/close.svg"
               />
             </div>
           </app-basic-info>
@@ -114,6 +114,7 @@ export default {
     position: absolute;
     right: 10px;
     top: 10px;
+    pointer-events: all;
     img {
       padding: 0 5px;
       cursor: pointer;
