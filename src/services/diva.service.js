186e36704fc26c8b029d60965b8aff68d00b393b
陈志强 <chenzhiqiang@corp.sheencity.com>
2022-06-08 15:14:43 +0800

    chore: 修改内嵌模式判定方式

--- src/services/diva.service.js
+++ src/services/diva.service.js
@@ -1,5 +1,4 @@
 import { Diva, Overlay } from '@sheencity/diva-sdk';
-
 import {
   Euler,
   EulerOrder,
@@ -19,16 +18,16 @@ export class DivaService {
    * @param {HTMLElement} element (HTMLElement) 视频加载的 dom 元素
    */
   async init(element) {
-    console.log({ element })
+    console.log({ element });
     let diva;
-    if(this.isEmbeddedMode()) {
+    if (this.isEmbeddedMode()) {
       // 使用内嵌模式
       diva = new Diva({
         mode: 'embedded',
         apiKey: 'xxx',
         container: element,
-      })
-    }else {
+      });
+    } else {
       // 使用云渲染模式
       diva = new Diva({
         mode: 'cloud',
@@ -36,14 +35,11 @@ export class DivaService {
         container: element,
         url: new URL('ws://127.0.0.1:3000'),
         logType: 'resolved',
-      })
+      });
     }
     console.log('diva is', diva);
-
     this.client = await diva.init();
-
     console.log('client is', this.client);
-
   }
 
   /**
@@ -51,7 +47,11 @@ export class DivaService {
    * @returns 内嵌模式下返回 true
    */
   isEmbeddedMode() {
-    return window.ue?.diva ? true : false;
+    if (window.navigator.userAgent.includes('Mars')) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /**
